328efa717cf33714f864ee27b4fbfa023a8b333e
isaacMS300 <102270561+IsaacMS30@users.noreply.github.com>
2025-11-29 12:54:31 -0600

    Added arguments for current command
    Also, added the steps needed to complete this command

--- src/weathercli/cli.py
+++ src/weathercli/cli.py
@@ -1,9 +1,17 @@
 import click
 
+# TODO(me): Do help commands
+
 @click.group()
 def cli():
     """WeatherCLI - A simple command line weather tool."""
 
 @cli.command()
-def hello():
-    click.echo("Hola! WeatherCLI está funcionando.")
+@click.argument('city', nargs = 1)
+@click.argument('country', nargs = 1)
+def current(city: str, country: str):
+    click.echo(f"Country: {country}, city: {city}")
+    # Get coordanates from the place
+    # Request information from API
+    # Format output
+
